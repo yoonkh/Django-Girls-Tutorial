9e2c39c13a624cd5a2c6c53a5b21cf01d46c83cf
yoonkh <zizou0812@gmail.com>
2017-05-29 16:46:11 +0900

    add Dynamic post Query in post_list view

--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -1,7 +1,17 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 def post_list(request):
+    # posts 변수에 ORM을 이용해서 Post의 리스트 ( 쿼리셋 )를 대입
+    post = Post.objects.filter(published_date__lte=timezone.now())
+
+
+    context = {
+        'title': 'Postlist from post_list view',
+        'posts': posts,
+    }
     # return HttpResponse('<html><body>Post List</body></html>')
-    return render(request, 'blog/post_list.html')
+    return render(request, 'blog/post_list.html', context=context)
 # Create your views here.
