c46827a242d0b36e2a78b969839bf400fb45f75b
yoonkh <zizou0812@gmail.com>
2017-05-29 15:22:16 +0900

    TEMPLATE_DIR setting, add post_list view

--- django_app/blog/views.py
+++ django_app/blog/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+def post_list(request):
+    # return HttpResponse('<html><body>Post List</body></html>')
+    return render(request, 'blog/post_list.html')
 # Create your views here.
--- django_app/mysite/settings.py
+++ django_app/mysite/settings.py
@@ -14,7 +14,10 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# os.path.join을 사용해서 TEMPLATE_DIR변수에 django_app/templates폴더의 경로를 할당
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
+print('TEMPLATE_DIR', TEMPLATE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -55,7 +58,9 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            TEMPLATE_DIR
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
